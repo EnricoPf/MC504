901c545109721309242c7dc02ab662a3fb837368
Augusto07 <joseaugusto.marcos@gmail.com>
2022-04-02 22:13:03 -0300

    first attempt

--- example.c
+++ example.c
@@ -1,36 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
+
+typedef struct {
+
+     char answer;
+     char try;
+     char correct;
+
+}char_block;
 
 void *function1( void *ptr );
 
-int main()
-{
-     pthread_t thread1, thread2;
-     char *message1 = "Thread 1";
-     char *message2 = "Thread 2";
-     int  iret1, iret2;
+char *answer = "poema";
 
-    /* Create independent threads each of which will execute function */
+int main(){
 
-     iret1 = pthread_create( &thread1, NULL, function1, (void*) message1);
-     iret2 = pthread_create( &thread2, NULL, function1, (void*) message2);
+     char_block* char1 = malloc(sizeof(char_block));
+     char_block* char2 = malloc(sizeof(char_block));
+     char_block* char3 = malloc(sizeof(char_block));
+     char_block* char4 = malloc(sizeof(char_block));
+     char_block* char5 = malloc(sizeof(char_block)); 
 
-     /* Wait till threads are complete before main continues. Unless we  */
-     /* wait we run the risk of executing an exit which will terminate   */
-     /* the process and all threads before the threads have completed.   */
+     char1->answer = answer[0];
+     char2->answer = answer[1];
+     char3->answer = answer[2];
+     char4->answer = answer[3];
+     char5->answer = answer[4];
 
-     pthread_join( thread1, NULL);
-     pthread_join( thread2, NULL); 
+     printf("Aperte T para começar:\n");
+     char mode;
+     scanf("%c", &mode);
+     
+     int attempts = 1;
+     printf("A palavra tem 5 letras\n");
 
-     printf("Thread 1 returns: %d\n",iret1);
-     printf("Thread 2 returns: %d\n",iret2);
-     exit(0);
+     while (mode == 't'){
+
+          char try[5];
+          printf("\n");
+          printf("Tentativa %d. Você possui %d tentativas restantes:\n", attempts, (8-attempts));
+          scanf("%s", try);
+
+          char1->try = try[0];
+          char2->try = try[1];
+          char3->try = try[2];
+          char4->try = try[3];
+          char5->try = try[4];
+
+          pthread_t thread1, thread2, thread3, thread4, thread5;
+          int  iret1, iret2, iret3, iret4, iret5;
+
+          iret1 = pthread_create( &thread1, NULL, function1, (void*) char1);
+          iret2 = pthread_create( &thread2, NULL, function1, (void*) char2);
+          iret3 = pthread_create( &thread3, NULL, function1, (void*) char3);
+          iret4 = pthread_create( &thread4, NULL, function1, (void*) char4);
+          iret5 = pthread_create( &thread5, NULL, function1, (void*) char5);
+
+          pthread_join( thread1, NULL);
+          pthread_join( thread2, NULL); 
+          pthread_join( thread3, NULL);
+          pthread_join( thread4, NULL);
+          pthread_join( thread5, NULL);
+
+          printf("%c,%c,%c,%c,%c", char1->correct, char2->correct, char3->correct, char4->correct, char5->correct);
+          printf("\n");
+          attempts+=1;
+     }
 }
 
-void *function1( void *ptr )
-{
-     char *message;
-     message = (char *) ptr;
-     printf("%s \n", message);
+void *function1( void *entry ){
+
+     char_block* char_;
+     char_ = (char_block*) entry;
+
+     if (char_->try == char_->answer){
+
+          char_->correct = 'C';
+     }
+     else if (strchr(answer,char_->try) != NULL){
+
+          char_->correct = 'A';
+     }
+     else{
+
+          char_->correct = 'W';
+     }
 }
