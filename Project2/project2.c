1d561593faef860b4c868cb87b0bbf39878f6411
EnricoPf <e233895@dac.unicamp.br>
2022-05-06 19:13:37 -0300

    started work on project2

---
+++ Project2/project2.c
@@ -0,0 +1,42 @@
+//Enrico Piovesana Fernandes RA 233895
+//Esse programa é uma adaptação do algoritmo do Jantar dos Selvagens
+//Esse algoritmo foi proposto na página 115 da 2° Edição do livro Little Book of Semaphores
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//N define o númnero de selvagens, sempre há apenas 1 chefe
+#define N 5
+//M é o número de refeições que o pote central consegue conter
+#define M 8
+#define MAX_CYCLES 10
+
+
+sem_t sem_pote[M];
+
+
+
+void* savage(void *aux){
+    //O selvagem segue o seguinte codigo base
+    //while True: 
+    // pegacomida()
+    // come()
+    return;
+}
+
+void* chef(void* aux){
+    //o chef segue o seguinte codigo base
+    //while True: 
+    // botacomida(M)
+}
+
+int main() {
+    pthread_t thr[N];
+    int sav_id[N];
+
+    sem_init(&sem_pote, M);
+
+}
